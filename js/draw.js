32ba857de302113f948cdb68f242a0814acdb3f4
Bhawna Agarwal <bhawna2109@users.noreply.github.com>
2020-04-18 12:47:05 -0400

    Add files via upload

--- js/draw.js
+++ js/draw.js
@@ -68,72 +68,72 @@ function groupAllData(data) {
 
 
 function visualizeMultiLineChart(dataitems) {
-      var margin = {top:20, right: 20, bottom:30, left:60}
-      var width = 940 - margin.left - margin.right
-      var height = 500 - margin.top - margin.bottom
-      var dates = [2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018]
+//       var margin = {top:20, right: 20, bottom:30, left:60}
+//       var width = 940 - margin.left - margin.right
+//       var height = 500 - margin.top - margin.bottom
+//       var dates = [2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018]
 
-      var x = d3.scaleUtc()
-            .domain(d3.extent(dates))
-            .range([margin.left, width - margin.right])
+//       var x = d3.scaleUtc()
+//             .domain(d3.extent(dates))
+//             .range([margin.left, width - margin.right])
 
-      var y = d3.scaleLinear()
-        .domain([0, d3.max(dataitems, (d) => d3.max(d.values))]).nice()
-        .range([height - margin.bottom, margin.top]);
+//       var y = d3.scaleLinear()
+//         .domain([0, d3.max(dataitems, (d) => d3.max(d.values))]).nice()
+//         .range([height - margin.bottom, margin.top]);
 
 
-      var xAxis = g => g
-            .attr("transform", `translate(0,${height - margin.bottom})`)
-            .call(d3.axisBottom(x).ticks(width / 80).tickSizeOuter(0))
+//       var xAxis = g => g
+//             .attr("transform", `translate(0,${height - margin.bottom})`)
+//             .call(d3.axisBottom(x).ticks(width / 80).tickSizeOuter(0))
 
       
-      var yAxis = g => g
-            .attr("transform", `translate(${margin.left},0)`)
-            .call(d3.axisLeft(y))
-            .call(g => g.select(".domain").remove())
-            .call(g => g.select(".tick:last-of-type text").clone()
-                  .attr("x", 3)
-                  .attr("text-anchor", "start")
-                  .attr("font-weight", "bold")
-                  .text(data.y))
+//       var yAxis = g => g
+//             .attr("transform", `translate(${margin.left},0)`)
+//             .call(d3.axisLeft(y))
+//             .call(g => g.select(".domain").remove())
+//             .call(g => g.select(".tick:last-of-type text").clone()
+//                   .attr("x", 3)
+//                   .attr("text-anchor", "start")
+//                   .attr("font-weight", "bold")
+//                   .text(data.y))
 
-      line = d3.line()
-            .defined(d => !isNaN(d))
-            .x((d, i) => x(dates[i]))
-            .y(d => y(d))
+//       line = d3.line()
+//             .defined(d => !isNaN(d))
+//             .x((d, i) => x(dates[i]))
+//             .y(d => y(d))
             
 
-      d3 = require("d3@5", "d3-array@2")
+//       d3 = require("d3@5", "d3-array@2")
 
       
-      const svg = d3.create("svg")
-            .attr("viewBox", [0, 0, width, height])
-            .style("overflow", "visible");
+//       const svg = d3.create("svg")
+//             .attr("viewBox", [0, 0, width, height])
+//             .style("overflow", "visible");
       
-      svg.append("g")
-            .call(xAxis);
+//       svg.append("g")
+//             .call(xAxis);
       
-      svg.append("g")
-            .call(yAxis);
+//       svg.append("g")
+//             .call(yAxis);
       
-      const path = svg.append("g")
-            .attr("fill", "none")
-            .attr("stroke", "steelblue")
-            .attr("stroke-width", 1.5)
-            .attr("stroke-linejoin", "round")
-            .attr("stroke-linecap", "round")
-            .selectAll("path")
-            .data(data.series)
-            .join("path")
-            .style("mix-blend-mode", "multiply")
-            .attr("d", d => line(d.values));
+//       const path = svg.append("g")
+//             .attr("fill", "none")
+//             .attr("stroke", "steelblue")
+//             .attr("stroke-width", 1.5)
+//             .attr("stroke-linejoin", "round")
+//             .attr("stroke-linecap", "round")
+//             .selectAll("path")
+//             .data(data.series)
+//             .join("path")
+//             .style("mix-blend-mode", "multiply")
+//             .attr("d", d => line(d.values));
       
 
-      var svg = d3.select("#chart1").append("svg")
-                  .attr("width", width + margin.left + margin.right)
-                  .attr("height", height + margin.top + margin.bottom)
-            .append("g")
-            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+//       var svg = d3.select("#chart1").append("svg")
+//                   .attr("width", width + margin.left + margin.right)
+//                   .attr("height", height + margin.top + margin.bottom)
+//             .append("g")
+//             .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 
       // svg.selectAll(".bar")
